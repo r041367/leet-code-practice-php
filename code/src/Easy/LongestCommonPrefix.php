d97d0f8c13722feab9f9ce30b8a03e0d22ffff80
murphy <murphy@katsumitec.com>
2021-09-14 00:49:27 +0800

    add "14. Longest Common Prefix"

---
+++ code/src/Easy/LongestCommonPrefix.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace R041367\LeetCodePracticePhp\Easy;
+
+/**
+ * 14. Longest Common Prefix
+ * https://leetcode.com/problems/longest-common-prefix/
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * Constraints:
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lower-case English letters.
+ *
+ * Class LongestCommonPrefix
+ * @package R041367\LeetCodePracticePhp\Easy
+ */
+class LongestCommonPrefix
+{
+    public static function run(array $strings): string
+    {
+    }
+}
---
+++ code/tests/Easy/LongestCommonPrefixTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Easy;
+
+use PHPUnit\Framework\TestCase;
+use R041367\LeetCodePracticePhp\Easy\LongestCommonPrefix;
+
+class LongestCommonPrefixTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     * @param  string[]  $strings
+     */
+    public function test_LongestCommonPrefix_WithDataProvider_AllPass(array $strings, string $expected): void
+    {
+        $actual = LongestCommonPrefix::run($strings);
+
+        self::assertEquals($expected, $actual);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [["flower", "flow", "flight"], 'fl'],
+            [["dog", "racecar", "car"], ''],
+        ];
+    }
+}
