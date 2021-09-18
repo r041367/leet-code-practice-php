a640a9f3ca871d41c60b86a698db05d0a1194b6e
r041367 <r041367@gmail.com>
2021-09-18 20:57:14 +0800

    add 168. Excel Sheet Column Title

---
+++ code/src/Easy/ExcelSheetColumnTitle.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace R041367\LeetCodePracticePhp\Easy;
+
+/**
+ * 168. Excel Sheet Column Title
+ * https://leetcode.com/problems/excel-sheet-column-title/
+ *
+ * Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+ *
+ * Constraints:
+ * 1 <= columnNumber <= 231 - 1
+ *
+ * Class ExcelSheetColumnTitle
+ * @package R041367\LeetCodePracticePhp\Easy
+ */
+class ExcelSheetColumnTitle
+{
+    public static function run(int $columnNumber): string
+    {
+        // quotient 商
+        // remainder 余数
+        $arr = range('A', 'Z');
+
+        $base = count($arr);
+
+        $str = '';
+        while ($columnNumber > 0) {
+            --$columnNumber;
+            $m = ($columnNumber) % $base;
+
+            $str = $arr[$m].$str;
+            $columnNumber = floor(($columnNumber) / $base);
+        }
+
+        return $str;
+    }
+}
---
+++ code/tests/Easy/ExcelSheetColumnTitleTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Easy;
+
+use PHPUnit\Framework\TestCase;
+use R041367\LeetCodePracticePhp\Easy\ExcelSheetColumnTitle;
+
+class ExcelSheetColumnTitleTest extends TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     * @param  int  $columnNumber
+     * @param  string  $expected
+     */
+    public function test_Run_WithDataProvider_AllPass(int $columnNumber, string $expected): void
+    {
+        $actual = ExcelSheetColumnTitle::run($columnNumber);
+
+        self::assertEquals($expected, $actual);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            'A,1' => [1, 'A'],
+            'Z,26' => [26, 'Z'],
+            'AB,28' => [28, 'AB'],
+            'AN,40' => [40, 'AN'],
+            'AZ,52' => [52, 'AZ'],
+            'BA,53' => [53, 'BA'],
+            'ZY,701' => [701, 'ZY'],
+            'FXSHRXW,2147483647' => [2147483647, 'FXSHRXW'],
+        ];
+    }
+}
